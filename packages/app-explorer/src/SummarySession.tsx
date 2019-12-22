992109d8e222a92ddc6e33c27eb649d6b26386a2
satellitex <s.a.t.e.3.ths@gmail.com>
2019-12-23 05:38:28 +0900

    add viewer epoch

--- packages/app-explorer/src/SummarySession.tsx
+++ packages/app-explorer/src/SummarySession.tsx
@@ -45,9 +45,12 @@ function renderSession ({ sessionInfo, t, withSession = true }: Props): React.Re
 }
 
 function renderEra ({ sessionInfo, t, withEra = true }: Props): React.ReactNode {
+  console.log('renderEra', sessionInfo, withEra);
   if (!withEra || !sessionInfo) {
+    console.log('null!')
     return null;
   }
+  console.log('ok!', sessionInfo.sessionLength.toString())
 
   const label = t('era');
 
@@ -70,13 +73,13 @@ function renderEra ({ sessionInfo, t, withEra = true }: Props): React.ReactNode 
 
 function SummarySession (props: Props): React.ReactElement<Props> {
   const { api } = useApi();
-  const sessionInfo = useCall<DerivedSessionInfo>(api.derive.session.info, []);
+  const sessionInfo = useCall<DerivedSessionInfo>(api.derive.plasmStaking.info, []);
   const [expanded, setExpanded] = useState<Props>(props);
 
   useEffect((): void => {
     setExpanded({ ...props, sessionInfo });
   }, [props, sessionInfo]);
-
+  
   return (
     <>
       {renderSession(expanded)}
